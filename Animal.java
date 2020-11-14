b258c86784d17c33df92cd979e02de53fee4f8c0
Maëlle <maelle.chaumont@gmail.com>
2020-11-14 16:37:05 +0100

    Ajout de la classe Animal

---
+++ Animal.java
@@ -0,0 +1,16 @@
+package TD7;
+
+public abstract class Animal {
+
+	private String nom ;
+	private int abs ;
+	private int ord ;
+	
+	
+	public void seDeplacer (int x, int y) {
+		abs+=x;
+		ord+=y; 
+	}
+	
+
+}
